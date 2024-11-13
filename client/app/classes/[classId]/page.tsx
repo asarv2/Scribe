1e53949b6dde7240570971ed88bdbbcfc8060508
Ashok Saravanan <ashko.saravanan@gmail.com>
2024-11-13 13:45:36 -0500

    Update page.tsx

--- client/app/classes/[classId]/page.tsx
+++ client/app/classes/[classId]/page.tsx
@@ -9,7 +9,7 @@
 import { useQuery } from "@tanstack/react-query";
 import { getLectures } from "../../../utils/queries/get-lectures";
 import useSupabaseBrowser from "../../../utils/supabase/supabase-browser";
-import { AspectRatio, Box, Button, Center, Container, em, Group, Input, LoadingOverlay, SimpleGrid, Stack, Text, useMantineTheme } from "@mantine/core";
+import { AspectRatio, Box, Button, Center, Container, em, Flex, Group, Input, LoadingOverlay, SimpleGrid, Skeleton, Stack, Text, useMantineTheme } from "@mantine/core";
 import { useEffect, useState } from "react";
 import { answerQuestion, answerSlideQuestion, } from "../../../utils/services/question";
 import { notifications } from '@mantine/notifications';
@@ -31,9 +31,9 @@ import { LINEAR_PROGRAMMING_MAP } from "@/utils/map/map-tree";
 
 export default function Class({ params }: { params: { classId: string } }) {
     const [opened, { open, close }] = useDisclosure(false)
-	const [openNodeLabel, setOpenNodeLabel] = useState<string>()
-	const [openNodeDescription, setOpenNodeDescription] = useState<string>()
-	const theme = useMantineTheme()
+    const [openNodeLabel, setOpenNodeLabel] = useState<string>()
+    const [openNodeDescription, setOpenNodeDescription] = useState<string>()
+    const theme = useMantineTheme()
     const pathname = usePathname()
 
     const supabase = useSupabaseBrowser();
@@ -65,7 +65,7 @@ export default function Class({ params }: { params: { classId: string } }) {
     return (
         <>
             <div style={{ position: "fixed", width: "100vw", zIndex: 100 }}>
-            <HeaderSimple />
+                <HeaderSimple />
             </div>
             {/* <Container fluid>
                 <SimpleGrid cols={5}>
@@ -78,10 +78,10 @@ export default function Class({ params }: { params: { classId: string } }) {
                     {textbooks?.map((textbook) => <Text>{textbook.title}</Text>)}
                 </SimpleGrid>
             </Container> */}
-            <div style={{width: "100vw", height: "100vh"}}>
+            <div style={{ width: "100vw", height: "100vh" }}>
                 <ReactFlowProvider>
-                <LoadingOverlay />
-					{/* <Map
+                    <LoadingOverlay />
+                    {/* <Map
 						key={query.dataUpdatedAt}
 						rootNode={data.output}
 						onNodeClick={(label, description) => {
@@ -96,12 +96,11 @@ export default function Class({ params }: { params: { classId: string } }) {
             </div>
 
             {/* Want a panel on the right hand side showing all of the lectures*/}
-            <div style={{ position: "fixed", left: "5vw", top: "10vh", width: "20vw", height: "50vh", backgroundColor: theme.primaryColor === "dark" ? theme.colors.dark[7] : theme.colors.gray[0], padding: 20, overflowY: "scroll" }}>
+            <div style={{ position: "fixed", left: "5vw", top: "10vh", backgroundColor: theme.primaryColor === "dark" ? theme.colors.dark[7] : theme.colors.gray[0], padding: 20, overflowY: "scroll" }}>
                 <SimpleGrid cols={1}>
-                    {slides?.map((slide) => <Link href={`${pathname}/slide/${slide.id}`}><Button> L{slide.note_number} - {slide.name}</Button></Link>)}
+                    {slides?.map((slide) => <Link href={`${pathname}/slide/${slide.id}`}><Button size={isMobile ? "compact-xs" : "md"}> L{slide.note_number} - {slide.name}</Button></Link>)}
                 </SimpleGrid>
             </div>
-
         </>
     );
 }
