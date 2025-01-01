f2e2896ef22515d2735bd9b5c7dd61a2cc7447ef
Ashok Saravanan <ashko.saravanan@gmail.com>
2025-01-01 17:08:39 -0600

    Update HeaderSimple.tsx

--- client/components/HeaderSimple.tsx
+++ client/components/HeaderSimple.tsx
@@ -16,7 +16,7 @@ import Image from 'next/image';
 const links = [
     { link: '/classes/3236bffb-cfa4-47b8-a0a2-44427df57e3b', label: 'MA 421' },
     // { link: '/classes/c068ccf8-4892-45b3-8dab-04d5d3aa85ad', label: 'CS 243' },
-    { link: '/login', label: 'Professor' },
+    // { link: '/login', label: 'Professor' },
 ];
 
 export function HeaderSimple() {
